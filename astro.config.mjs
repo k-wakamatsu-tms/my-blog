f79aa3e717c6931cccdea1e2444ce823e2ff58cc
k-wakamatsu-tms <69659176+k-wakamatsu-tms@users.noreply.github.com>
2024-07-17 21:18:13 +0900

    chore: Update site configuration and content for Kouta.W's Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,29 @@
-import {defineConfig} from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import {remarkModifiedTime} from "./src/utils/remark-modified-time";
-import {siteConfig} from "./src/config";
+import { remarkModifiedTime } from "./src/utils/remark-modified-time";
+import { siteConfig } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.site,
-    integrations: [mdx(), sitemap(), tailwind({
-        applyBaseStyles: false
-    }), icon()],
-    markdown: {
-        shikiConfig: {
-            themes: {
-                light: 'one-light',
-                dark: 'one-dark-pro'
-            }
-        },
-        remarkPlugins: [remarkModifiedTime]
+  site: siteConfig.site,
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "one-light",
+        dark: "one-dark-pro",
+      },
     },
-    devToolbar: {
-        enabled: false
-    }
+    remarkPlugins: [remarkModifiedTime],
+  },
 });
