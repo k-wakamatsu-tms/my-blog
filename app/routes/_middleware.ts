455c02ddfa0d8d527f2bbab95e8e9cab9d7e035a
k-wakamatsu-tms <69659176+k-wakamatsu-tms@users.noreply.github.com>
2024-07-17 19:01:50 +0900

    chore: replace astro

--- app/routes/_middleware.ts
+++
@@ -1,5 +0,0 @@
-import { createRoute } from "honox/factory";
-import { logger } from "hono/logger";
-import { secureHeaders } from "hono/secure-headers";
-
-export default createRoute(logger(), secureHeaders());
